10c9e1f1354b085a382fafa1fade7772bd8f94d7
juliet29 <jnwagwuume-ezeoke@college.harvard.edu>
2018-06-05 11:41:31 +0100

    mobile adjustments2

--- application.py
+++ application.py
@@ -77,5 +77,5 @@ def errorhandler(e):
 
 
 # listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
+#for code in default_exceptions:
+    #app.errorhandler(code)(errorhandler)
